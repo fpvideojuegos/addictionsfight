306f16cc391d885753943c4d888cf6ec10c64b98
Malomarq <malomarq@gmail.com>
2019-12-05 10:31:43 +0100

    repositorio actualizado 100%

--- src/scenes/UI.js
+++ src/scenes/UI.js
@@ -36,8 +36,8 @@ class UI extends Phaser.Scene {
         });
 
         //Cofre que lleva a la ventana de inventario
-        const inventoryButton = this.add.image(this.width - 210, 8 , GameConstants.UI.INVENTORY)
-                    .setScrollFactor(0).setDepth(10).setOrigin(0).setAlpha(1).setScale(0.2);
+        const inventoryButton = this.add.image(this.width - 190, 8 , GameConstants.UI.INVENTORY)
+                    .setScrollFactor(0).setDepth(10).setOrigin(0).setAlpha(1).setScale();
         inventoryButton.setInteractive();
 
         const playAgainButton = this.add.image(this.width - 140, 15 , GameConstants.Sprites.PlayAgain.KEY)
